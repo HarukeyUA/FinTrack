edf58f4ac38940834c13d25c7ff2df4285cf28ad
HarukeyUA <inazarrusnak@gmail.com>
2021-03-05 14:04:08 +0200

    Initial navigation graph

---
+++ app/src/main/java/com/harukeyua/fintrack/OverviewFragment.kt
@@ -0,0 +1,21 @@
+package com.harukeyua.fintrack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+
+class OverviewFragment : Fragment() {
+
+    private val viewModel: OverviewViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.overview_fragment, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/harukeyua/fintrack/OverviewViewModel.kt
@@ -0,0 +1,7 @@
+package com.harukeyua.fintrack
+
+import androidx.lifecycle.ViewModel
+
+class OverviewViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
