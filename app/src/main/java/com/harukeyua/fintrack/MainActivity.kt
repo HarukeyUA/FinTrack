145539ac0bea5ddfcfda63305e880ab8657cfc75
HarukeyUA <inazarrusnak@gmail.com>
2021-03-20 12:37:20 +0200

    Add bottom navigation support
    This will include: Overview, Stats, stats on map and sync

--- app/src/main/java/com/harukeyua/fintrack/MainActivity.kt
+++ app/src/main/java/com/harukeyua/fintrack/MainActivity.kt
@@ -2,12 +2,21 @@ package com.harukeyua.fintrack
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
+
 }
